c6074a9674b35f036cab26b92543dccad84555d4
Paul Duda <paulduda@cisco.com>
2018-02-21 13:47:45 -0500

    Add proxy environment for offsite logstash plugin build.

--- mirror/create_mirror_misc.sh
+++ mirror/create_mirror_misc.sh
@@ -9,17 +9,50 @@ fi
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
 
 STATIC_FILE_LIST=$(<${MIRROR_BUILD_DIR}/dependencies/pnda-static-file-dependencies.txt)
+STATIC_FILE_DIR=$MIRROR_OUTPUT_DIR/mirror_misc
 PLUGIN_LIST=$(<${MIRROR_BUILD_DIR}/dependencies/pnda-logstash-plugin-dependencies.txt)
 source ${MIRROR_BUILD_DIR}/common/utils.sh
 
-STATIC_FILE_DIR=$MIRROR_OUTPUT_DIR/mirror_misc
+
+handle_logstash() {
+    local tarfile=$1
+    local software_name=${tarfile%.tar.gz}
+    local software_version=${software_name#*-}
+    local plugins_file_name="logstash-offline-plugins-${software_version}.zip"
+
+    if [ -n "$http_proxy" ];
+    then
+        local hp=${http_proxy##*//}
+        PROXY_HOST=${hp%*:*}
+        PROXY_PORT=${hp#*:*}
+        export JRUBY_OPTS="-J-Dhttp.proxyHost=${PROXY_HOST} -J-Dhttp.proxyPort=${PROXY_PORT}"
+    fi
+
+    tar xzf $tarfile -C /tmp
+    cd /tmp/$software_name
+    # work around bug introduced in 5.1.1: https://discuss.elastic.co/t/5-1-1-plugin-installation-behind-proxy/70454
+    JARS_SKIP='true' bin/logstash-plugin install $PLUGIN_LIST
+    bin/logstash-plugin prepare-offline-pack $PLUGIN_LIST
+    chmod a+r $plugins_file_name
+    mv $plugins_file_name $STATIC_FILE_DIR/$plugins_file_name
+}
+
+
 mkdir -p $STATIC_FILE_DIR
 cd $STATIC_FILE_DIR
 echo "$STATIC_FILE_LIST" | while read STATIC_FILE
 do
+    h=$(dirname "$STATIC_FILE")
+    d=${h#*//*/}
+    f=${STATIC_FILE##*/}
     echo $STATIC_FILE
     robust_curl "$STATIC_FILE"
+    if [[ $f =~ logstash.*\.tar\.gz$ ]];
+    then
+        handle_logstash $f
+    fi
 done
+# TODO: Get rid of these static names
 cat SHASUMS256.txt | grep node-v6.10.2-linux-x64.tar.gz > node-v6.10.2-linux-x64.tar.gz.sha1.txt
 sha512sum je-5.0.73.jar > je-5.0.73.jar.sha512.txt
 sha512sum Anaconda2-4.0.0-Linux-x86_64.sh > Anaconda2-4.0.0-Linux-x86_64.sh.sha512.txt
@@ -32,13 +65,3 @@ elif [ "x$DISTRO" == "xubuntu" ]; then
     apt-get install -y libpq-dev
 fi
 
-cd /tmp
-robust_curl https://artifacts.elastic.co/downloads/logstash/logstash-5.2.2.tar.gz
-tar zxf logstash-5.2.2.tar.gz
-rm logstash-5.2.2.tar.gz
-cd logstash-5.2.2
-# work around bug introduced in 5.1.1: https://discuss.elastic.co/t/5-1-1-plugin-installation-behind-proxy/70454
-JARS_SKIP='true' bin/logstash-plugin install $PLUGIN_LIST
-bin/logstash-plugin prepare-offline-pack $PLUGIN_LIST
-chmod a+r logstash-offline-plugins-5.2.2.zip
-mv logstash-offline-plugins-5.2.2.zip $STATIC_FILE_DIR/logstash-offline-plugins-5.2.2.zip
