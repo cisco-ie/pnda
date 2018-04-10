4960effa5aee2771a1862320841ce7b2bd19abac
Paul Duda <paulduda@cisco.com>
2018-04-10 18:23:10 +0000

    Add setuptools version for Xenial.

--- mirror/create_mirror_python.sh
+++ mirror/create_mirror_python.sh
@@ -13,12 +13,23 @@ elif [ "x$DISTRO" == "xubuntu" ]; then
     apt-get install -y libffi-dev python3-pip gcc
 fi
 
+OS_VERSION=$(lsb_release -sr)
+OS_ID=$(lsb_release -si)
+OS_SHORT=${OS_ID,,}${OS_VERSION%.*}
+
+if [ "$OS_SHORT" == "Ubuntu16" ];
+then
+    SETUPTOOLS_VERSION='34.3.3'
+else
+    SETUPTOOLS_VERSION='34.2.0'
+fi
+
 curl -LOJf https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
-pip2 install setuptools==34.2.0
+pip2 install setuptools==${SETUPTOOLS_VERSION}
 pip2 install github3.py
 python3 get-pip.py
-pip3 install setuptools==34.2.0
+pip3 install setuptools==${SETUPTOOLS_VERSION}
 rm get-pip.py
 
 python $MIRROR_BUILD_DIR/tools/python_download_packages.py
